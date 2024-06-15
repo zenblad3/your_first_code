8d18803b126b84e709003c6449c7facbea2d8b61
zenblad3 <lrm951520@gmail.com>
2024-06-14 20:33:05 -0400

    Update main.c
    holbertonschool apply

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
+#include <stdio.h>
+int main() {
+for (int i =0; i < 32; i++)
+    
+    printf ("Hello, School\n");
   return 0;
 }
